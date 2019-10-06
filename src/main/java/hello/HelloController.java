04ca9df2611088e42860cdbe84c4485584e5834c
Junyi Du <junyi_du@cs.ucsb.edu>
2019-10-06 15:42:19 -0700

    first change

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
+    
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+
+        String html = "<h1>Hello World!</h1>\n" +
+            "<p>This web app is powered by \n" +
+            "<a href='https://github.com/heathcliffdu/spring-boot-minimal-webapp'>this github repo</a></p>\n";
+        return html;
     }
-    
 }
+
